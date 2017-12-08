cf45f98515f5ed92bfc58a38c3c443627ec9829a
MAshraf <ee.mashraf@gmail.com>
2017-12-08 09:34:54 +0200

    Home view

---
+++ src/views/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
+
+class Home extends Component {
+  componentWillMount () {
+    this.props.fetchData()
+  }
+
+  render () {
+    return (
+      <div>
+        {this.props.posts &&
+          Object.values(this.props.posts).map(post => (
+            <h1 key={post.id}>{post.title.rendered}</h1>
+          ))}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  posts: state.receivePosts
+})
+
+const mapDispatchToProps = dispatch => ({
+  dispatch,
+  fetchData: () => dispatch(fetchPosts())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
