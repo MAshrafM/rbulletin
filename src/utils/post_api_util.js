8dd3442ab04d60360753c4c31990e9a7287b839e
MAshraf <ee.mashraf@gmail.com>
2017-12-08 10:28:13 +0200

    fix cors for development

--- src/utils/post_api_util.js
+++ src/utils/post_api_util.js
@@ -1,6 +1,9 @@
 // Get posts from wordpress
 // Using Torrent Freak
 export const fetchPosts = () =>
-  fetch('https://torrentfreak.com/wp-json/wp/v2/posts').then(data =>
-    data.json()
-  )
+  fetch('https://torrentfreak.com/wp-json/wp/v2/posts', {
+    mode: 'cors',
+    header: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  }).then(data => data.json())
