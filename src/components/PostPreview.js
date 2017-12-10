5072919f31214e8358fad8d664c22a52712e17f0
MAshraf <ee.mashraf@gmail.com>
2017-12-10 16:54:36 +0200

    PostPreview

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,24 +1,57 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+// Material UI
+import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
+import Button from 'material-ui/Button'
+import Typography from 'material-ui/Typography'
+import { withStyles } from 'material-ui/styles'
 
-class PostPreview extends Component {
-  render () {
-    const date = new Date(this.props.date).toLocaleString()
-
-    return (
-      <div style={{ border: '1px solid black', padding: '5px' }}>
-        <h1>{this.props.title}</h1>
-        <img
-          alt=''
-          style={{ height: '500px', width: '500px' }}
-          src={this.props.imageURL}
-        />
-        <br />
-        <Link to={`/post/${this.props.id}`}>Read More</Link>
-        <h5>Date: {date}</h5>
-      </div>
-    )
+const styles = {
+  card: {
+    marginBottom: 15
+  },
+  media: {
+    height: 500
   }
 }
 
-export default PostPreview
+class PostPreview extends Component {
+  constructor () {
+    super()
+    this.renderCard = this.renderCard.bind(this)
+    this.transitionToPost = this.transitionToPost.bind(this)
+  }
+
+  renderCard () {
+    const classes = this.props.classes
+    const date = new Date(this.props.date).toLocaleString()
+
+    return (
+      <Card className={classes.card}>
+        <CardMedia className={classes.media} image={this.props.imageURL} />
+        <CardContent>
+          <Typography type='headline' component='h2'>
+            {this.props.title}
+          </Typography>
+          <Typography type='subheading' component='h3'>
+            {date}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button dense color='primary' onClick={this.transitionToPost}>
+            Read More
+          </Button>
+        </CardActions>
+      </Card>
+    )
+  }
+
+  transitionToPost () {
+    this.props.history.push(`/posts/${this.props.id}`)
+  }
+  render () {
+    return <div>{this.renderCard()}</div>
+  }
+}
+
+export default withStyles(styles)(withRouter(PostPreview))
