b58d52a70e9f44abf4738cc319336fdf0dcdcac3
MAshraf <ee.mashraf@gmail.com>
2018-01-02 17:50:57 +0200

    hide drawer

--- src/components/Layout/drawer.js
+++ src/components/Layout/drawer.js
@@ -6,6 +6,7 @@ import Divider from 'material-ui/Divider'
 import IconButton from 'material-ui/IconButton'
 import { ListItem, ListItemText, ListItemIcon } from 'material-ui/List'
 import ListSubheader from 'material-ui/List/ListSubheader'
+import Hidden from 'material-ui/Hidden'
 // material-ui-icon
 import ChevronLeftIcon from 'material-ui-icons/ChevronLeft'
 import AlarmClock from 'material-ui-icons/Alarm'
@@ -16,60 +17,79 @@ import { getAllCategories } from '../../graphql/queries/categories'
 class SideComponent extends Component {
   render () {
     const classes = this.props.classes
-    return (
-      <div>
-        <Drawer
-          type='persistent'
-          classes={{
-            paper: classes.drawerPaper
-          }}
-          open={this.props.open}
-        >
-          <div className={classes.drawerInner}>
-            <div className={classes.drawerHeader}>
-              <Link to='/'>
-                <ListItem>
-                  <img
-                    className={classes.logo}
-                    alt='logo'
-                    src='https://raw.githubusercontent.com/dabbott/react-native-express/master/static/logo.png'
-                  />
-                </ListItem>
-              </Link>
-              <IconButton onClick={this.props.handleDrawerClose}>
-                <ChevronLeftIcon />
-              </IconButton>
-            </div>
-            <Divider />
-            <ListSubheader>Current Events</ListSubheader>
-            <Link to='/' className={classes.link}>
+    const drawer = (
+      <div className={classes.drawerInner}>
+        <div className={classes.drawerHeader}>
+          <ListItem>
+            <img
+              className={classes.image}
+              alt='logo'
+              src='https://raw.githubusercontent.com/dabbott/react-native-express/master/static/logo.png'
+            />
+          </ListItem>
+          <IconButton
+            className={classes.navIconHide}
+            onClick={this.props.handleDrawerClose}
+          >
+            <ChevronLeftIcon />
+          </IconButton>
+        </div>
+        <Divider />
+        <ListSubheader>Current Events</ListSubheader>
+        <Link to='/' className={classes.link}>
+          <ListItem button>
+            <ListItemIcon>
+              <AlarmClock />
+            </ListItemIcon>
+            <ListItemText inset secondary='Home' />
+          </ListItem>
+        </Link>
+        <Divider />
+        <ListSubheader>Categories</ListSubheader>
+        {!this.props.data.loading &&
+          this.props.data.categories &&
+          this.props.data.categories.edges.map(category => (
+            <Link
+              key={category.node.id}
+              to={`/category/${category.node.slug}`}
+              className={classes.link}
+            >
               <ListItem button>
-                <ListItemIcon>
-                  <AlarmClock />
-                </ListItemIcon>
-                <ListItemText inset secondary='Home' />
+                <ListItemText secondary={category.node.name} />
               </ListItem>
             </Link>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary='Categories' />
-            </ListItem>
-            {!this.props.data.loading &&
-              this.props.data.categories &&
-              this.props.data.categories.edges.map(category => (
-                <Link
-                  key={category.node.id}
-                  to={`/category/${category.node.slug}`}
-                  className={classes.link}
-                >
-                  <ListItem button>
-                    <ListItemText secondary={category.node.name} />
-                  </ListItem>
-                </Link>
-              ))}
-            <Divider />
-          </div>
-        </Drawer>
+          ))}
+        <Divider />
+      </div>
+    )
+    return (
+      <div>
+        <Hidden mdUp>
+          <Drawer
+            type='temporary'
+            open={this.props.open}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            onRequestClose={this.props.handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden mdDown implementation='css'>
+          <Drawer
+            type='permanent'
+            open
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
       </div>
     )
   }
