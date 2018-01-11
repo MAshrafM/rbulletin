51fb01a15922d91751c2b49d9d9414854f790821
MAshraf <ee.mashraf@gmail.com>
2018-01-11 17:25:09 +0200

    add attachment

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 // Material UI
 import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card'
 import Button from 'material-ui/Button'
 import Typography from 'material-ui/Typography'
 import { withStyles } from 'material-ui/styles'
 import { blue, grey } from 'material-ui/colors'
+import Divider from 'material-ui/Divider'
+// Material UI Icons
+import AttachmentIcon from 'material-ui-icons/Attachment'
 
 const styles = {
   card: {
@@ -58,12 +60,14 @@ class PostPreview extends Component {
             <CardMedia className={classes.media} image={this.props.imageURL} />
           )}
         </CardContent>
+        <Divider />
         <CardActions>
-          <Link className={classes.link} to={`/post/${this.props.id}`}>
-            <Button dense color='primary'>
-              Read More
-            </Button>
-          </Link>
+          <Button href={`/post/${this.props.id}`} dense color='primary'>
+            Read More
+          </Button>
+          <Button dense color='primary'>
+            <AttachmentIcon /> Attachment
+          </Button>
         </CardActions>
       </Card>
     )
